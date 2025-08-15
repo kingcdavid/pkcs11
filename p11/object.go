5af7f994ee2181a29c1065a7509eaec0656706b9
David King <david.king@globalsign.com>
2025-08-15 12:58:37 +0100

    Adding debug

--- p11/object.go
+++ p11/object.go
@@ -2,8 +2,8 @@ package p11
 
 import (
 	"errors"
+	"fmt"
 
-	"github.com/containerd/continuity"
 	"github.com/kingcdavid/pkcs11"
 )
 
@@ -52,10 +52,10 @@ func (o Object) Attribute(attributeType uint) ([]byte, error) {
 	// attribute. We don't consider that an error, we just consider that
 	// equivalent to an empty value.
 	if err == pkcs11.Error(pkcs11.CKR_ATTRIBUTE_TYPE_INVALID) {
-		continuity.Log("PKCS11: Attribute not found:", attributeType)
+		fmt.Errorf("PKCS11: Attribute not found:", attributeType)
 		return nil, nil
 	} else if err != nil {
-		continuity.Log("PKCS11: Attribute not found:", attributeType)
+		fmt.Errorf("PKCS11: Attribute not found:", attributeType)
 		return nil, err
 	}
 	if len(attrs) == 0 {
